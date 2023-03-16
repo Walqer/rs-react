c9694e7fd5aeb3626c9b0d1bfde3291bf6a34ce6
Walqer <32606809+Walqer@users.noreply.github.com>
2023-03-16 15:42:13 +0600

    feat: add header links

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import About from './pages/About';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
