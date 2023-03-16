6ded3b12a7d38492c2692726cef536b0b1e74cf5
Walqer <32606809+Walqer@users.noreply.github.com>
2023-03-16 12:00:07 +0600

    feat: add about page to router and change hash router to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import About from './pages/About';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
 
 export function WrappedApp() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
