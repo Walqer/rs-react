47b89eab9b0c17bc656c1eb3718c5a138c4524bb
Alimossim <agaev.alimosim@gmail.com>
2023-03-26 20:05:24 +0600

    feat: add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,20 +3,12 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { WrappedApp, App } from './App';
+import { App } from './App';
+import FormPage from './pages/FormPage';
 
 global.fetch = fetch as never;
 
 describe('App', () => {
-  it('Renders hello world', () => {
-    render(<WrappedApp />);
-    expect(
-      screen.getByRole('heading', {
-        level: 1,
-      })
-    ).toHaveTextContent('Hello these are random people in the world');
-  });
-
   it('Renders not found if invalid path', () => {
     render(
       <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
@@ -28,5 +20,20 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('Not Found');
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(<FormPage />);
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Form page');
+  });
+
+  it('should render the form', () => {
+    render(<FormPage />);
+    const form = screen.getByText('Add person');
+    expect(form).toBeInTheDocument();
   });
 });
