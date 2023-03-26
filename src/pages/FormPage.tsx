9cb9fd1347b7fc33d36988d7b8b3a18887450cda
Alimossim <agaev.alimosim@gmail.com>
2023-03-26 19:02:02 +0600

    feat: add modal

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -2,9 +2,11 @@
 import React from 'react';
 import { Card } from '../components/FormCard';
 import { FormProps, Form } from '../components/Form';
+import Modal from '../components/Modal';
 
 interface FormPageState {
   cardList: FormProps[];
+  modalVisibility: boolean;
 }
 
 class FormPage extends React.Component<unknown, FormPageState> {
@@ -12,23 +14,36 @@ class FormPage extends React.Component<unknown, FormPageState> {
     super(props);
     this.state = {
       cardList: [],
+      modalVisibility: true,
     };
   }
+
+  closeModal = () => {
+    this.setState({
+      modalVisibility: false,
+    });
+  };
 
   createCard = (card: FormProps) => {
     const { cardList } = this.state;
     this.setState({
       cardList: [...cardList, card],
+      modalVisibility: true,
     });
   };
 
   render() {
-    const { cardList } = this.state;
+    const { cardList, modalVisibility } = this.state;
     return (
       <>
         <h1>Form page</h1>
         <section className="form-section">
           <Form createCard={this.createCard} />
+          <Modal
+            modalVisibility={modalVisibility}
+            closeModal={this.closeModal}
+            modalText="Your form has been added"
+          />
           <ul className="card-list">
             {cardList.map((card) => {
               return <Card key={card.curentTime} cardData={card} />;
