daaefbdcf7e67f0dbb6105eac03fd95a91c04d23
Alimossim <agaev.alimosim@gmail.com>
2023-04-01 23:51:42 +0600

    refactor: migrate to funtional components

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,58 +1,40 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../components/FormCard';
 import { FormProps, Form } from '../components/Form';
 import Modal from '../components/Modal';
 
-interface FormPageState {
-  cardList: FormProps[];
-  modalVisibility: boolean;
-}
+function FormPage() {
+  const [cardList, setCardList] = useState<FormProps[]>([]);
+  const [modalVisibility, setModalVisibility] = useState(false);
 
-class FormPage extends React.Component<unknown, FormPageState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      cardList: [],
-      modalVisibility: false,
-    };
-  }
-
-  closeModal = () => {
-    this.setState({
-      modalVisibility: false,
-    });
+  const closeModal = () => {
+    setModalVisibility(false);
   };
 
-  createCard = (card: FormProps) => {
-    const { cardList } = this.state;
-    this.setState({
-      cardList: [...cardList, card],
-      modalVisibility: true,
-    });
+  const createCard = (card: FormProps) => {
+    setModalVisibility(true);
+    setCardList((prev) => [...prev, card]);
   };
 
-  render() {
-    const { cardList, modalVisibility } = this.state;
-    return (
-      <>
-        <h1>Form page</h1>
-        <section className="form-section">
-          <Form createCard={this.createCard} />
-          <Modal
-            modalVisibility={modalVisibility}
-            closeModal={this.closeModal}
-            modalText="Your form has been added"
-          />
-          <ul className="card-list">
-            {cardList.map((card) => {
-              return <Card key={card.createdTime} cardData={card} />;
-            })}
-          </ul>
-        </section>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Form page</h1>
+      <section className="form-section">
+        <Form createCard={createCard} />
+        <Modal
+          modalVisibility={modalVisibility}
+          closeModal={closeModal}
+          modalText="Your form has been added"
+        />
+        <ul className="card-list">
+          {cardList.map((card) => {
+            return <Card key={card.createdTime} cardData={card} />;
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
 
 export default FormPage;
