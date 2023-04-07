150d8668b115415c6313b54ef9ed62dd1ad20cc7
Alimossim <agaev.alimosim@gmail.com>
2023-04-07 23:09:17 +0600

    feat: add preloader

--- src/components/CharacterCards.tsx
+++ src/components/CharacterCards.tsx
@@ -1,29 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import Character from '../interfaces/Character';
+import preloader from '../assets/Hourglass.gif';
 
 interface CharacterCardsProps {
   searchQuery: string;
 }
 
 function CharacterCards(props: CharacterCardsProps) {
-  const [characters, setCharacters] = useState<Character[]>([]);
+  const [characters, setCharacters] = useState<Character[] | null>(null);
+  const [isPending, setIsPending] = useState(true);
   const { searchQuery } = props;
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?name=${searchQuery}`)
       .then((response) => response.json())
-      .then((data) => setCharacters(data.results));
+      .then((data) => setCharacters(data.results))
+      .then(() => setIsPending(false));
+
+    return () => {
+      setIsPending(true);
+      setCharacters(null);
+    };
   }, [searchQuery]);
 
   return (
-    <ul className="user-list">
-      {characters.map((person) => (
-        <li className="user-card" key={person.id}>
-          <img src={person.image} alt={`${person.name}`} />
-          <p>{person.name}</p>
-          <p>{person.location.name}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      {isPending && (
+        <div className="preloader">
+          <img width={256} height={256} src={preloader} alt="preloader" />
+          <span>Loading...</span>
+        </div>
+      )}
+      {characters && (
+        <ul className="user-list">
+          {characters.map((person) => (
+            <li className="user-card" key={person.id}>
+              <img src={person.image} alt={`${person.name}`} />
+              <p>{person.name}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
