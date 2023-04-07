0c4ff2b45045bcc78de6611fe1bd8a65392d95fb
Alimossim <agaev.alimosim@gmail.com>
2023-04-08 00:26:59 +0600

    refactor: refactor home page and modules

--- src/components/CharacterCards.tsx
+++ src/components/CharacterCards.tsx
@@ -1,46 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Character from '../interfaces/Character';
-import preloader from '../assets/Hourglass.gif';
 
 interface CharacterCardsProps {
-  searchQuery: string;
+  characters: Character[] | null;
 }
 
 function CharacterCards(props: CharacterCardsProps) {
-  const [characters, setCharacters] = useState<Character[] | null>(null);
-  const [isPending, setIsPending] = useState(true);
-  const { searchQuery } = props;
-  useEffect(() => {
-    fetch(`https://rickandmortyapi.com/api/character/?name=${searchQuery}`)
-      .then((response) => response.json())
-      .then((data) => setCharacters(data.results))
-      .then(() => setIsPending(false));
-
-    return () => {
-      setIsPending(true);
-      setCharacters(null);
-    };
-  }, [searchQuery]);
-
+  const { characters } = props;
   return (
-    <>
-      {isPending && (
-        <div className="preloader">
-          <img width={256} height={256} src={preloader} alt="preloader" />
-          <span>Loading...</span>
-        </div>
-      )}
-      {characters && (
-        <ul className="user-list">
-          {characters.map((person) => (
-            <li className="user-card" key={person.id}>
-              <img src={person.image} alt={`${person.name}`} />
-              <p>{person.name}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
+    <ul className="user-list">
+      {characters?.map((person) => (
+        <li className="user-card" key={person.id}>
+          <img src={person.image} alt={`${person.name}`} />
+          <p>{person.name}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../components/Search';
 import CharacterCards from '../components/CharacterCards';
+import Character from '../interfaces/Character';
+import preloader from '../assets/Hourglass.gif';
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -8,11 +10,31 @@ function Home() {
     setSearchQuery(query);
   };
 
+  const [characters, setCharacters] = useState<Character[] | null>(null);
+  const [isPending, setIsPending] = useState(true);
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${searchQuery}`)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .then(() => setIsPending(false));
+
+    return () => {
+      setIsPending(true);
+      setCharacters(null);
+    };
+  }, [searchQuery]);
+
   return (
     <>
       <h1 className="main-title">Hello these are random people in the world</h1>
       <Search onSearch={hadleSearch} />
-      <CharacterCards searchQuery={searchQuery} />;
+      {isPending && (
+        <div className="preloader">
+          <img width={256} height={256} src={preloader} alt="preloader" />
+          <span>Loading...</span>
+        </div>
+      )}
+      {characters && <CharacterCards characters={characters} />}
     </>
   );
 }
